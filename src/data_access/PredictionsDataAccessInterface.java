ca59b7a2b919cb981954af888e9b1fc724d5da2b
kuzeyozturac <kuzeyozturac2@gmail.com>
2023-10-30 16:35:09 -0400

    Implemented PredictionsInteractor. Added PredictionsDataAccessInterface but not finished implementing it fully.

---
+++ src/data_access/PredictionsDataAccessInterface.java
@@ -0,0 +1,14 @@
+package data_access;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+//TODO: Finish me
+
+public interface PredictionsDataAccessInterface {
+    /**
+     *
+     * @return
+     */
+    public HashMap<String, ArrayList<Integer>> getAllDirectionsAndArrivals();
+}
---
+++ src/use_case/PredictionsInteractor.java
@@ -0,0 +1,28 @@
+package use_case;
+
+import data_access.PredictionsDataAccessInterface;
+import data_access.StopDataAccessInterface;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PredictionsInteractor implements PredictionsInputBoundary{
+    final PredictionsDataAccessInterface predictionsDataAccessInterface;
+    final PredictionsOutputBoundary predictionsPresenter;
+
+    public PredictionsInteractor(PredictionsDataAccessInterface predictionsDataAccessInterface, PredictionsOutputBoundary predictionsPresenter){
+        this.predictionsDataAccessInterface = predictionsDataAccessInterface;
+        this.predictionsPresenter = predictionsPresenter;
+    }
+    @Override
+    public void execute(String stopTag) {
+        HashMap<String, ArrayList<Integer>> directionToArrivals = predictionsDataAccessInterface.getAllDirectionsAndArrivals();
+        if (directionToArrivals.isEmpty()){
+            predictionsPresenter.prepareFailView("Could not find any predictions.");
+        }
+        else{
+            PredictionsOutputData predictionsOutputData = new PredictionsOutputData(directionToArrivals);
+            predictionsPresenter.prepareSuccessView(predictionsOutputData);
+        }
+    }
+}
