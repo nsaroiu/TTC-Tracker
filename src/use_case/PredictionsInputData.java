17cc5a791e453b8a11be6e98c9d4cb4e9d413297
kuzeyozturac <kuzeyozturac2@gmail.com>
2023-10-30 23:41:10 -0400

    Added PredictionsInputData

---
+++ src/use_case/PredictionsInputData.java
@@ -0,0 +1,9 @@
+package use_case;
+
+public class PredictionsInputData {
+    private final String stop;
+    public PredictionsInputData(String stop){
+        this.stop = stop;
+    }
+    public String getStop(){return stop;}
+}
