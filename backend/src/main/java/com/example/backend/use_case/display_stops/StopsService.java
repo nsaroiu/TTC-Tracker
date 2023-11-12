4dd1c0b5138af8645d005607802af89e956a3e49
Prashanth Reddy Shyamala <prashanth.shyamala@mail.utoronto.ca>
2023-11-12 18:20:26 -0500

    Created StopsController and StopsService for the API endpoint for the Show Stops use-case.

---
+++ backend/src/main/java/com/example/backend/use_case/display_stops/StopsService.java
@@ -0,0 +1,19 @@
+package com.example.backend.use_case.display_stops;
+
+import com.example.backend.data_access.stop.StopDataAccessInterface;
+import com.example.backend.entity.Location;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class StopsService {
+    @Autowired
+    private StopDataAccessInterface stopDataAccessInterface;
+
+    public HashMap<String, Location> execute() {
+        HashMap<String, Location> allStops = stopDataAccessInterface.getAllStopTagsAndLocations();
+        return allStops;
+    }
+}
