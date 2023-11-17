fa3da2620343943e74a3541a891d6b12f9ff0df6
Prashanth Reddy Shyamala <prashanth.shyamala@mail.utoronto.ca>
2023-11-17 15:05:47 -0500

    Renamed StopsService and StopsImplementation as StopsImplementation implements StopsService

--- backend/src/main/java/com/example/backend/use_case/display_stops/StopsImplementation.java
+++ backend/src/main/java/com/example/backend/use_case/display_stops/StopsImplementation.java
@@ -1,9 +1,19 @@
 package com.example.backend.use_case.display_stops;
 
+import com.example.backend.data_access.stop.StopDataAccessInterface;
 import com.example.backend.entity.Location;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
-public interface StopsImplementation {
-    public HashMap<String, Location> execute();
+@Service
+public class StopsImplementation implements StopsService {
+    @Autowired
+    private StopDataAccessInterface stopDataAccessObject;
+
+    public HashMap<String, Location> execute() {
+        HashMap<String, Location> allStops = stopDataAccessObject.getAllStopTagsAndLocations();
+        return allStops;
+    }
 }
--- backend/src/main/java/com/example/backend/use_case/display_stops/StopsService.java
+++ backend/src/main/java/com/example/backend/use_case/display_stops/StopsService.java
@@ -7,13 +7,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
-@Service
-public class StopsService implements StopsImplementation {
-    @Autowired
-    private StopDataAccessInterface stopDataAccessObject;
-
-    public HashMap<String, Location> execute() {
-        HashMap<String, Location> allStops = stopDataAccessObject.getAllStopTagsAndLocations();
-        return allStops;
-    }
+public interface StopsService {
+    public HashMap<String, Location> execute();
 }
