66e6f7a65f96c38270182cabd8b316e9e94e7fb3
kuzeyozturac <kuzeyozturac2@gmail.com>
2023-11-17 14:42:54 -0500

    Added @CrossOrigin(origins = "*")

--- backend/src/main/java/com/example/backend/use_case/stop_details/StopDetailsController.java
+++ backend/src/main/java/com/example/backend/use_case/stop_details/StopDetailsController.java
@@ -1,15 +1,13 @@
 package com.example.backend.use_case.stop_details;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * This is the controller for the stop details use case.
  */
 @RestController
+@CrossOrigin(origins = "*")
 public class StopDetailsController {
     /**
     *  Instance variables:
