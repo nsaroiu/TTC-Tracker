1fee562f9391c287d7fc638eae7d95c93352d721
prash-red <shyamalaprashanth2004@gmail.com>
2023-11-18 01:59:09 -0500

    added functionality to cluster stop points upto a zoom level and just show the stops after that zoom level

--- frontend/view/map.tsx
+++ frontend/view/map.tsx
@@ -1,10 +1,10 @@
-import React, {useState, useMemo, useCallback, useRef, useEffect, MutableRefObject} from "react";
+import React, {useState, useMemo, useCallback, useRef, useEffect} from "react";
 import {
-  GoogleMap,
-  Marker,
-  DirectionsRenderer,
-  Circle,
-  MarkerClusterer,
+    GoogleMap,
+    Marker,
+    DirectionsRenderer,
+    Circle,
+    MarkerClusterer,
 } from "@react-google-maps/api";
 
 import {Stops} from "../interface_adapter/display_stops/StopsData";
@@ -16,11 +16,12 @@ type DirectionsResult = google.maps.DirectionsResult;
 type MapOptions = google.maps.MapOptions
 
 export default function Map() {
-  const mapRef = useRef<google.maps.Map>();
-  const center = useMemo<LatLngLiteral>(
-    () => ({ lat: 43.6532, lng: -79.3832 }),
-    []
-  );
+    const mapRef = useRef<google.maps.Map>();
+    const center = useMemo<LatLngLiteral>(
+        () => ({lat: 43.6532, lng: -79.3832}),
+        []
+    );
+    const [mapZoom, setZoom] = useState<number>(10);
     const {getStops} = StopsController();
     const [stops, setStops] = useState<Stops>();
     const [visibleStops, setVisibleStops] = useState<Array<LatLngLiteral>>([]);
@@ -28,11 +29,14 @@ export default function Map() {
         const updateVisibleStops = () => {
             if (mapRef.current) {
                 const bounds = mapRef.current.getBounds();
-                const zoom = mapRef.current.getZoom();
-                console.log(bounds, zoom);
+                const newZoom = mapRef.current.getZoom();
+                if (newZoom) {
+                    setZoom(newZoom);
+                }
+                console.log(bounds, newZoom)
                 let filteredStops: Stops = {};
-                if (zoom) {
-                    if (zoom >= 14.5) {
+                if (newZoom) {
+                    if (newZoom >= 14.5) {
                         filteredStops = Object.keys(stops || {}).reduce((acc, key) => {
                             if (!stops) {
                                 return acc;
@@ -74,16 +78,23 @@ export default function Map() {
     }, [stops]);
 
 
-  const options = useMemo<MapOptions>(
-    () => ({
-      mapId: "b181cac70f27f5e6",
-      disableDefaultUI: true,
-      clickableIcons: false,
-    }),
-    []
-  );
-  const onLoad = useCallback((map) => (mapRef.current = map), []);
-  const handleGetStops = useCallback(() => {
+    const options = useMemo<MapOptions>(
+        () => ({
+            mapId: "b181cac70f27f5e6",
+            disableDefaultUI: true,
+            clickableIcons: false,
+        }),
+        []
+    );
+    const onLoad = useCallback((map) => {
+        mapRef.current = map;
+        if (mapRef.current) {
+            setZoom(mapRef.current.getZoom() || 10);
+        }
+    }, []);
+
+
+    const handleGetStops = useCallback(() => {
         getStops().then((stops) => {
                 setStops(stops);
                 console.log(stops);
@@ -92,36 +103,50 @@ export default function Map() {
     }, []);
 
 
-  // @ts-ignore
-  return (
-    <div className="container">
-        <div className="controls">
-            <button onClick={handleGetStops}>Get Stops</button>
+    // @ts-ignore
+    return (
+        <div className="container">
+            <div className="controls">
+                <button onClick={handleGetStops}>Get Stops</button>
+            </div>
+            <div className="map">
+                <GoogleMap
+                    zoom={10}
+                    center={center}
+                    mapContainerClassName="map-container"
+                    options={options}
+                    onLoad={onLoad}
+                >
+                    {visibleStops.length > 0 && (
+                        mapZoom && mapZoom >= 17 ? (
+                            // Display individual markers if zoom is greater than or equal to 16
+                            visibleStops.map((stop, index) => (
+                                <Marker
+                                    key={index}
+                                    position={stop}
+                                    icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+                                />
+                            ))
+                        ) : (
+                            // Use marker clustering if zoom is less than 16
+                            <MarkerClusterer>
+                                {(clusterer) =>
+                                    visibleStops.map((stop, index) => (
+                                        <Marker
+                                            key={index}
+                                            position={stop}
+                                            icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+                                            clusterer={clusterer}
+                                        />
+                                    ))
+                                }
+                            </MarkerClusterer>
+                        )
+                    )}
+
+
+                </GoogleMap>
+            </div>
         </div>
-      <div className="map">
-        <GoogleMap
-          zoom={10}
-          center={center}
-          mapContainerClassName="map-container"
-          options={options}
-          onLoad={onLoad}
-        >
-            {visibleStops.length > 0 && (
-                <MarkerClusterer>
-                    {(clusterer) =>
-                        visibleStops.map((stop, index) => (
-                            <Marker
-                                key={index}
-                                position={stop}
-                                icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-                                clusterer={clusterer}
-                            />
-                        ))
-                    }
-                </MarkerClusterer>
-            )}
-        </GoogleMap>
-      </div>
-    </div>
-  );
+    );
 }
