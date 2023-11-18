c085d081e1c62facd311d64a5c714a710e9a4d08
prash-red <shyamalaprashanth2004@gmail.com>
2023-11-18 12:16:30 -0500

    modified getStops to return a Display stops type object instead of StopsData type

--- frontend/interface_adapter/display_stops/StopsController.tsx
+++ frontend/interface_adapter/display_stops/StopsController.tsx
@@ -1,8 +1,8 @@
 import {useCallback} from "react";
-import {StopsData} from "./StopsData";
+import {StopsData, DisplayStopsData} from "./StopsData";
 
 const StopsController = () => {
-    const fetchData = async () => {
+    const fetchData = async (): Promise<StopsData|undefined> => {
         try {
             const response = await fetch('http://localhost:8080/stops'); // Replace with your API endpoint
             if (!response.ok) {
@@ -17,15 +17,31 @@ const StopsController = () => {
         }
     };
 
-    const getStops = useCallback(async (): Promise<StopsData|undefined> => {
+    const getStops = useCallback(async (): Promise<DisplayStopsData | undefined> => {
         try {
             // Wait for the fetchData promise to resolve
-            return await fetchData();
+            const data: StopsData | undefined = await fetchData();
+            if (!data) {
+                return undefined;
+            }
+
+            const stops: DisplayStopsData = {};
+            Object.keys(data).forEach((key: string) => {
+                stops[key] = {
+                    lat: data[key].latitude,
+                    lng: data[key].longitude,
+                };
+            });
+
+            return stops;
         } catch (error: any) {
             // Handle errors if the fetchData promise is rejected
             console.error('Error fetching data:', error.message);
+            return undefined; // Make sure to return something in case of an error
         }
     }, []);
+
+
 
     return {
         getStops
