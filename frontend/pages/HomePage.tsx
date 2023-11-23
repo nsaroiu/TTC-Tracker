22ba0eedcce93bd0a5bc6d272998959f28ae79d8
prash-red <shyamalaprashanth2004@gmail.com>
2023-11-23 01:58:22 -0500

    Modified HomePage to not route to /map when the continue button is clicked

--- frontend/pages/HomePage.tsx
+++ frontend/pages/HomePage.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Link from "next/link";
 
-const HomePage: React.FC = () => {
+interface HomePageProps {
+    updateClicked: (clicked: boolean) => void;
+}
+
+const HomePage: React.FC<HomePageProps> = ({updateClicked}) => {
     return (
         <>
         <link href="https://fonts.cdnfonts.com/css/manolo-mono" rel="stylesheet"></link>
         <div className="home-container">
             <h1 className="title">TTC Tracker</h1>
             <div className="button-container">
-                <Link href="/map">
-                    <a className="continue-button">Continue</a>
-                </Link>
+                <a className="continue-button" onClick={() => updateClicked(true)}>Continue</a>
             </div>
         </div>
         </>
