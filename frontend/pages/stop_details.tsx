0ac7f14491aebcc305a36e1d73930f008f9d162e
prash-red <shyamalaprashanth2004@gmail.com>
2023-11-25 17:09:55 -0500

    Added functionality to create a dropdown box to display the directions of each route in stop_details.tsx

--- frontend/pages/stop_details.tsx
+++ frontend/pages/stop_details.tsx
@@ -1,19 +1,46 @@
 // StopsDetails.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import StopDetailsOutputData from "../interface_adapter/stop_details/StopDetailsData";
 
 interface StopsDetailsProps {
-    stopDetails: StopDetailsOutputData; // Import StopDetailsOutputData interface
+    stopDetails: StopDetailsOutputData;
 }
 
 const StopsDetails: React.FC<StopsDetailsProps> = ({ stopDetails }) => {
+    const [selectedRoute, setSelectedRoute] = useState<string | null>(null);
+
+    const handleRouteChange = (routeTag: string) => {
+        setSelectedRoute(routeTag);
+    };
+
     // Implement your UI to display stopDetails in an info window
     return (
         <div>
             <h3>{stopDetails.stopName}</h3>
-    {/* Display additional details as needed */}
-    </div>
-);
+            {Object.keys(stopDetails.routeTagsToDir).map((routeTag) => (
+                <div key={routeTag}>
+                    <h4>{routeTag}</h4>
+                    <div>
+                        <label>Select Direction:</label>
+                        <select onChange={(e) => handleRouteChange(e.target.value)} value={selectedRoute || ""}>
+                            <option value="" disabled>Select a direction</option>
+                            {Object.entries(stopDetails.routeTagsToDir[routeTag]).map(([dirTag, directionName]) => (
+                                <option key={dirTag} value={dirTag}>
+                                    {directionName}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    {selectedRoute === routeTag && (
+                        <div>
+                            <h5>Selected Direction: {stopDetails.routeTagsToDir[routeTag][selectedRoute]}</h5>
+                            {/* Additional content for the selected direction */}
+                        </div>
+                    )}
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default StopsDetails;
