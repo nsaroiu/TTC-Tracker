8a606573838f248c895c0868529840a67fc430ef
prash-red <shyamalaprashanth2004@gmail.com>
2023-11-25 22:25:28 -0500

    Modified stops.tsx to account for the new data interfaces in StopsData.tsx

--- frontend/pages/stops.tsx
+++ frontend/pages/stops.tsx
@@ -2,8 +2,6 @@ import { Marker, MarkerClusterer } from "@react-google-maps/api";
 import React from "react";
 import { DisplayStopsData } from "../interface_adapter/display_stops/StopsData";
 
-type LatLngLiteral = google.maps.LatLngLiteral;
-
 interface StopsProps {
     visibleStops: DisplayStopsData;
     mapZoom: number;
@@ -13,27 +11,26 @@ interface StopsProps {
 const Stops: React.FC<StopsProps> = ({ visibleStops, mapZoom, updateSelectedStop }) => {
     return (
         <>
-            {visibleStops && Object.keys(visibleStops).length > 0 && (
+            {visibleStops && visibleStops.length > 0 && (
                 mapZoom && mapZoom >= 17 ? (
                     // Display individual markers if zoom is greater than or equal to 16
-                    Object.keys(visibleStops).map((key: string) => (
+                    visibleStops.map((stop, index) => (
                         <Marker
-                            key={key}
-                            position={{ lat: visibleStops[key].lat, lng: visibleStops[key].lng }}
-                            onClick={() => updateSelectedStop(key)}
+                            key={stop.tag}
+                            position={{ lat: stop.location.lat, lng: stop.location.lng }}
+                            onClick={() => updateSelectedStop(stop.tag)}
                             icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
                         />
-
                     ))
                 ) : (
                     // Use marker clustering if zoom is less than 16
                     <MarkerClusterer>
                         {(clusterer) => (
-                            Object.keys(visibleStops).map((key) => (
+                            visibleStops.map((stop, index) => (
                                 <Marker
-                                    key={key}
-                                    position={{ lat: visibleStops[key].lat, lng: visibleStops[key].lng }}
-                                    onClick={() => updateSelectedStop(key)}
+                                    key={stop.tag}
+                                    position={{ lat: stop.location.lat, lng: stop.location.lng }}
+                                    onClick={() => updateSelectedStop(stop.tag)}
                                     icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
                                     clusterer={clusterer}
                                 />
