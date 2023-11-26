fc0978716c76a4b53a480ee026d2386edfa6067f
prash-red <shyamalaprashanth2004@gmail.com>
2023-11-25 22:25:15 -0500

    Modified StopsController.tsx to account for the new data interfaces in StopsData.tsx

--- frontend/interface_adapter/display_stops/StopsController.tsx
+++ frontend/interface_adapter/display_stops/StopsController.tsx
@@ -1,51 +1,46 @@
-import {useCallback} from "react";
-import {StopsData, DisplayStopsData} from "./StopsData";
+import { useCallback } from "react";
+import { StopsData, DisplayStopsData, StopObject } from "./StopsData";
 
 const StopsController = () => {
-    const fetchData = async (): Promise<StopsData|undefined> => {
+    const fetchData = async (): Promise<StopsData | undefined> => {
         try {
-            const response = await fetch('http://localhost:8080/stops'); // Replace with your API endpoint
+            const response = await fetch("http://localhost:8080/stops");
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
 
             return await response.json();
-            // Do something with the data, e.g., update state, show in UI, etc.
         } catch (error: any) {
-            console.error('Error fetching data:', error.message);
-            // Handle error, e.g., show an error message to the user
+            console.error("Error fetching data:", error.message);
         }
     };
 
     const getStops = useCallback(async (): Promise<DisplayStopsData | undefined> => {
         try {
-            // Wait for the fetchData promise to resolve
             const data: StopsData | undefined = await fetchData();
             if (!data) {
                 return undefined;
             }
 
-            const stops: DisplayStopsData = {};
-            Object.keys(data).forEach((key: string) => {
-                stops[key] = {
-                    lat: data[key].latitude,
-                    lng: data[key].longitude,
-                };
-            });
+            const stops: DisplayStopsData = data.stops.map((stop: StopObject) => ({
+                name: stop.name,
+                tag: stop.tag,
+                location: {
+                    lat: stop.location.latitude,
+                    lng: stop.location.longitude,
+                },
+            }));
 
             return stops;
         } catch (error: any) {
-            // Handle errors if the fetchData promise is rejected
-            console.error('Error fetching data:', error.message);
-            return undefined; // Make sure to return something in case of an error
+            console.error("Error fetching data:", error.message);
+            return undefined;
         }
-    }, []);
-
-
+    }, [fetchData]);
 
     return {
-        getStops
-    }
-}
+        getStops,
+    };
+};
 
 export default StopsController;
