8682b06bb3e0ab3b0d803299d7450833bbd87a78
kuzeyozturac <kuzeyozturac2@gmail.com>
2023-11-27 16:01:34 -0500

    Modified the StopDetailsImplementation such that a direction is only added if it serves our stopTag.

--- backend/src/main/java/com/example/backend/use_case/stop_details/StopDetailsImplementation.java
+++ backend/src/main/java/com/example/backend/use_case/stop_details/StopDetailsImplementation.java
@@ -7,6 +7,8 @@ import com.example.backend.entity.Stop;
 import com.example.backend.entity.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -33,8 +35,10 @@ public class StopDetailsImplementation implements StopDetailsService {
      * @return The output data containing a mapping of routeTags to a mapping of direction tags to direction names
      */
     public StopDetailsOutputData execute(String stopTag){
+
         //Get a set of all stops
         HashSet<Stop> allStops = stopDataAccessObject.getAllStops();
+
         //Initialize the stop name
         String stopName = "Empty";
         for (Stop stop : allStops){ // Check each stop
@@ -43,36 +47,53 @@ public class StopDetailsImplementation implements StopDetailsService {
                 stopName = stop.getName();
             }
         }
+
         // If still empty, make the stopTag the name (this will never be satisfied, but is included to be safe)
         if (stopName.equals("Empty")){
             stopName = stopTag;
         }
+
         //Initialize the mapping (maps routeTags to a mapping of dirTags to dirName)
         HashMap<String, HashMap<String, String>> routeTagsToDir = new HashMap<>();
+
         //Use the stopTag to get the routeTags
         HashSet<String> routeTags = routeDataAccessObject.getRouteTagsByStopTag(stopTag);
         for (String routeTag : routeTags){
+
             //Use the routeTag to get the route
             Route route = routeDataAccessObject.getRouteByRouteTag(routeTag);
+
             //Use the route to get its direction tags
             HashMap<String, RouteDirection> directions = route.getRouteDirections();
             HashSet<String> dirTags = new HashSet<>();
             dirTags.addAll(directions.keySet());
             HashMap<String, String> dirTagsToNames = new HashMap<>();
+
             //Extract direction tag and direction name
             for (String dirTag : dirTags){
                 RouteDirection direction = directions.get(dirTag);
-                String dirName = direction.getName();
-                dirTagsToNames.put(dirTag, dirName);
+                //Look at every stopTag this direction serves
+                ArrayList<String> dirStopTags = direction.getStops();
+
+                //Only add the dirTag to dirName if the direction serves our stopTag
+                if (dirStopTags.contains(stopTag)) {
+                    String dirName = direction.getName();
+                    dirTagsToNames.put(dirTag, dirName);
+                }
             }
 
-            //Map the routeTag to the mapping of direction tags to direction names
-            routeTagsToDir.put(routeTag, dirTagsToNames);
+            //Only map the routeTag to the mapping of direction tags to direction names
+            // if the mapping of direction tags to direction names is not empty
+            if (!dirTagsToNames.isEmpty()){
+                routeTagsToDir.put(routeTag, dirTagsToNames);
+            }
+
         }
 
         //Initialize new outputData with the stopName and the route tags mapped to a mapping of
         // direction tags to direction names
         StopDetailsOutputData outputData = new StopDetailsOutputData(stopName, routeTagsToDir);
+
         //Return the outputData
         return outputData;
     }
