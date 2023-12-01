276e4917184e3dab0647f4b6b2e262e5c8930e6a
prash-red <shyamalaprashanth2004@gmail.com>
2023-12-01 00:55:51 -0500

    Reset route selection in StopsDetails component on stopDetails change
    In the StopsDetails component, added a useEffect hook to reset selectedRoute, selectedDirection, and selectedRouteTag to null when the stopDetails prop changes. This ensures a clean state when a new stop is selected, allowing users to choose a route and direction as if it's the first selection.

--- frontend/pages/stop_details.tsx
+++ frontend/pages/stop_details.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import StopDetailsOutputData from "../interface_adapter/stop_details/StopDetailsData";
 import { RouteParam } from "../interface_adapter/route_details/RouteDetailsData";
 
@@ -10,7 +10,7 @@ interface StopsDetailsProps {
 const StopsDetails: React.FC<StopsDetailsProps> = ({ stopDetails, UpdateSelectedRoute }) => {
     const [selectedRoute, setSelectedRoute] = useState<string | null>(null);
     const [selectedDirection, setSelectedDirection] = useState<string | null>(null);
-    const [dirTag, setDirTag] = useState<string | null>(null);
+    const [selectedRouteTag, setSelectedRouteTag] = useState<string | null>(null);
 
     const handleRouteChange = (routeTag: string, dirTag: string, dirName: string) => {
         setSelectedRoute(routeTag);
@@ -18,35 +18,57 @@ const StopsDetails: React.FC<StopsDetailsProps> = ({ stopDetails, UpdateSelected
         UpdateSelectedRoute({ routeTag, dirTag });
     };
 
+    useEffect(() => {
+        setSelectedRouteTag(null);
+        setSelectedRoute(null);
+        setSelectedDirection(null);
+    }, [stopDetails]);
+
     // Implement your UI to display stopDetails in an info window
     return (
         <div>
             <h3>{stopDetails.stopName}</h3>
-            {Object.keys(stopDetails.routeTagsToDir).map((routeTag) => (
-                <div key={routeTag}>
-                    <h4>{routeTag}</h4>
-                    <div>
-                        <label>Select Direction:</label>
-                        <select
-                            onChange={(e) => {
-                                const dirTag:string = e.target.value;
-                                const dirName = stopDetails.routeTagsToDir[routeTag][dirTag];
-                                setDirTag(dirTag); // Update dirTag directly
-                                setSelectedDirection(dirName); // Update selectedDirection directly
-                                handleRouteChange(routeTag, dirTag, dirName);
-                            }}
-                            value={dirTag || ""}
-                        >
-                            <option value="" disabled>Select a direction</option>
-                            {Object.entries(stopDetails.routeTagsToDir[routeTag]).map(([dirTag, directionName]) => (
-                                <option key={dirTag} value={dirTag}>
-                                    {directionName}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
+            <div>
+                <label>Select Route Tag:</label>
+                <select
+                    onChange={(e) => {
+                        const routeTag = e.target.value;
+                        setSelectedRouteTag(routeTag);
+                        setSelectedRoute(null);
+                        setSelectedDirection(null);
+                    }}
+                    value={selectedRouteTag || ""}
+                >
+                    <option value="" disabled>Select a route tag</option>
+                    {Object.keys(stopDetails.routeTagsToDir).map((routeTag) => (
+                        <option key={routeTag} value={routeTag}>
+                            {routeTag}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            {selectedRouteTag && stopDetails.routeTagsToDir[selectedRouteTag] && (
+                <div>
+                    <label>Select Direction:</label>
+                    <select
+                        onChange={(e) => {
+                            const dirTag: string = e.target.value;
+                            const dirName = stopDetails.routeTagsToDir[selectedRouteTag][dirTag];
+                            setSelectedRoute(selectedRouteTag);
+                            setSelectedDirection(dirName);
+                            handleRouteChange(selectedRouteTag, dirTag, dirName);
+                        }}
+                        value={""}
+                    >
+                        <option value="" disabled>Select a direction</option>
+                        {Object.entries(stopDetails.routeTagsToDir[selectedRouteTag]).map(([dirTag, directionName]) => (
+                            <option key={dirTag} value={dirTag}>
+                                {directionName}
+                            </option>
+                        ))}
+                    </select>
                 </div>
-            ))}
+            )}
             {stopDetails && selectedRoute && selectedDirection && (
                 <div>
                     <h5>Selected Direction: {selectedDirection}</h5>
