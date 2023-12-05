b344af7dce022e6c2734ed92ef8d57c0014652f0
kuzeyozturac <kuzeyozturac2@gmail.com>
2023-12-04 20:15:44 -0500

    Organized the tests such that it is 100% for ScheduleStrategy. Do not know why CalculateDistanceStrategy is Line 42%.

--- backend/src/test/java/com/example/backend/use_case/predictions/strategies/StrategyInputDataTest.java
+++ backend/src/test/java/com/example/backend/use_case/predictions/strategies/StrategyInputDataTest.java
@@ -1,6 +1,7 @@
 package com.example.backend.use_case.predictions.strategies;
 
 import com.example.backend.entity.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -11,55 +12,64 @@ import static org.junit.Assert.assertEquals;
 
 public class StrategyInputDataTest {
 
-    @Test
-    public void testExecute() {
+    StrategyInputData strategyInputData;
+    Route route;
+    String DIRTAG = "North";
+    float AVERAGE_SPEED = 10.0f;
+    String STOPTAG = "A";
+    ArrayList<String> schedule = new ArrayList<>();
+    ArrayList<Location> shape = new ArrayList<>();
+    HashMap<String, Stop> stops = new HashMap<String, Stop>();
+    HashSet<String> routeTags = new HashSet<String>();
+    ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
+    Strategy scheduleStrategy = new ScheduleStrategy();
+
+    Strategy calculateDistanceStrategy = new CalculateDistanceStrategy();
+
+
+
+    @Before
+    public void setUp(){
         //Instantiate the builder
         StrategyInputDataBuilder strategyInputDataBuilder = new StrategyInputDataBuilder();
 
         //set the schedule
-        ArrayList<String> schedule = new ArrayList<>();
         schedule.add("18:30:00");
         strategyInputDataBuilder.setSchedule(schedule);
 
         //set the dirTag
-        String DIRTAG = "North";
         strategyInputDataBuilder.setDirTag(DIRTAG);
 
         //set the average speed
-        float AVERAGE_SPEED = 10.0f;
         strategyInputDataBuilder.setAvgSpeed(AVERAGE_SPEED);
 
         //set the shape
-        ArrayList<Location> shape = new ArrayList<>();
         shape.add(new Location(1,1));
         strategyInputDataBuilder.setShape(shape);
 
         //set the stops
-        HashMap<String, Stop> stops = new HashMap<String, Stop>();
-
         //set the routeTags
-        HashSet<String> routeTags = new HashSet<String>();
         routeTags.add("100");
-        String STOPTAG = "A";
         stops.put(STOPTAG,new Stop(STOPTAG,STOPTAG,1.0f,1.0f,routeTags));
         ArrayList<String> stops2 = new ArrayList<>();
         stops2.add(STOPTAG);
 
         //set the routeDirections
         HashMap<String, RouteDirection> routeDirections = new HashMap<String, RouteDirection>();
-        routeDirections.put("A",new RouteDirection(stops2,DIRTAG,"A"));
-        Route route = new Route(stops, "routeTag", routeDirections);
+        routeDirections.put(DIRTAG,new RouteDirection(stops2,DIRTAG,"A"));
+        route = new Route(stops, "routeTag", routeDirections);
         strategyInputDataBuilder.setRoute(new Route(stops, "routeTag", routeDirections));
         strategyInputDataBuilder.setStopTag("A");
 
         //set the vehicles
-        ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
         vehicles.add(new Vehicle(1,1,1.0f,1.0f,1,DIRTAG));
         strategyInputDataBuilder.setVehicles(vehicles);
 
         //Build the strategyInputData
-        StrategyInputData strategyInputData = strategyInputDataBuilder.build();
-
+        strategyInputData = strategyInputDataBuilder.build();
+    }
+    @Test
+    public void testInputData() {
         //Assert the average speed is correct
         assertEquals(AVERAGE_SPEED, strategyInputData.getAvgSpeed(), 1);
 
@@ -82,5 +92,19 @@ public class StrategyInputDataTest {
         assertEquals(STOPTAG, strategyInputData.getStopTag());
 
     }
+
+    @Test
+    public void testScheduleStrategyExecute(){
+        ArrayList<Float> result = new ArrayList<>();
+        result.add(1353.1666f);
+        assertEquals(result.get(0),scheduleStrategy.execute(strategyInputData).get(0),100);
+    }
+
+    @Test
+    public void testCalculateDistanceExecute(){
+        ArrayList<Float> result = new ArrayList<>();
+        assertEquals(result, calculateDistanceStrategy.execute(strategyInputData));
+    }
+
 
 }
