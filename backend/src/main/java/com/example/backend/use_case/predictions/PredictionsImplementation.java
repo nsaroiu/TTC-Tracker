fcedd0fd494800af0af5a7ff069df77559e9acb2
Rimmy50 <nsaroiu@hotmail.com>
2023-12-06 21:46:06 -0500

    Changed PredictionsImplementation.setStrategy() from private to public.
    Added testing for PredictionsImplementation.

--- backend/src/main/java/com/example/backend/use_case/predictions/PredictionsImplementation.java
+++ backend/src/main/java/com/example/backend/use_case/predictions/PredictionsImplementation.java
@@ -6,7 +6,6 @@ import com.example.backend.data_access.vehicle.VehicleDataAccessInterface;
 import com.example.backend.data_access.direction.DirectionDataAccessInterface;
 import com.example.backend.entity.Location;
 import com.example.backend.entity.Route;
-import com.example.backend.entity.RouteDirection;
 import com.example.backend.entity.Vehicle;
 import com.example.backend.use_case.predictions.strategies.CalculateDistanceStrategy;
 import com.example.backend.use_case.predictions.strategies.ScheduleStrategy;
@@ -31,7 +30,7 @@ public class PredictionsImplementation implements PredictionsService {
     private DirectionDataAccessInterface directionDAO;
     private Strategy strategy;
 
-    private void setStrategy(Strategy strategy) {
+    public void setStrategy(Strategy strategy) {
         this.strategy = strategy;
     }
 
