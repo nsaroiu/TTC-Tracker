013eaa2634c901d3f4652aa72ee7d7f7f425f0cd
Rimmy50 <nsaroiu@hotmail.com>
2023-12-06 20:26:22 -0500

    ScheduleStrategyTest now mocks LocalDateTime.

--- backend/src/test/java/com/example/backend/use_case/predictions/strategies/ScheduleStrategyTest.java
+++ backend/src/test/java/com/example/backend/use_case/predictions/strategies/ScheduleStrategyTest.java
@@ -1,19 +1,42 @@
 package com.example.backend.use_case.predictions.strategies;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.MockitoAnnotations;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class ScheduleStrategyTest {
+
+    private static MockedStatic<LocalDateTime> mockedLocalDateTime;
+    private static LocalDateTime localDateTime = LocalDateTime.of(2021, 1, 1, 8, 0, 0, 1);
+
+    @InjectMocks
+    private StrategyInputData data;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        mockedLocalDateTime = mockStatic(LocalDateTime.class);
+
+        mockedLocalDateTime.when(LocalDateTime::now).thenReturn(localDateTime);
+
+    }
 
     @Test
     void execute_shouldReturnClosestTimes() {
         // Mock data setup
-        StrategyInputData data = mock(StrategyInputData.class);
+        data = mock(StrategyInputData.class);
         when(data.getSchedule()).thenReturn(getSampleSchedule());
 
         // Create an instance of the ScheduleStrategy
@@ -25,6 +48,11 @@ class ScheduleStrategyTest {
         // Verify the interaction
         assertEquals(3, predictions.size());
         // Add your specific assertions based on the sample schedule and current time
+    }
+
+    @AfterEach
+    void close() {
+        mockedLocalDateTime.close();
     }
 
     private ArrayList<String> getSampleSchedule() {
